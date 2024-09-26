c80958a449a56f04ec4196ac70cea0f09f0bc5c9
lgoncalves94 <goncalves.leonardo@proton.me>
2024-09-26 19:55:14 +0200

    cleaned API Response set Dataframes and updated requirements

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import json
+import pandas as pd
 from utils import *
 
 get_weather_input()
-
 with open('weather_data.txt','r') as file:
     parsed_data = json.load(file)
 
-current_data_point = parsed_data['location'] | parsed_data['current']
-
-print(current_data_point)
+full_dict = parsed_data['location'] | parsed_data['current']
+df = clean_df_inplace(full_dict)
+print(df)
